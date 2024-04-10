8dc50150a50085cb6edae542b9a4f978ed3a3bb2
V.V.M Team <miro.vvmteam@gmail.com>
2024-04-10 11:56:59 +0700

    Update

---
+++ type/js/081.js
@@ -0,0 +1,26 @@
+var questionsDatabase = [
+  {
+    question: "Các cuộc khởi nghĩa thời Bắc thuộc đều nhằm mục tiêu khôi phục nền độc lập, tự chủ.",
+    options: ["Đúng.", "Sai."],
+    correctAnswer: 1,
+    id: 1
+  },
+  {
+    question: "Các cuộc khởi nghĩa đều giành được chính quyền và khôi phục lại nền độc lập, tự chủ",
+    options: ["Đúng.", "Sai."],
+    correctAnswer: 2,
+	id: 2
+  },
+  {
+    question: "Tinh thần yêu nước độc lập tự chủ quyết định sự tồn tại của dân tộc trong thời Bắc thuộc.",
+    options: ["Đúng.", "Sai."],
+    correctAnswer: 2,
+	id: 3
+  },
+  {
+    question: "Chỉ có các cuộc khởi nghĩa thời chống Bắc thuộc mới mang tính chất dân tộc sâu sắc.",
+    options: ["Đúng.", "Sai."],
+    correctAnswer: 2,
+	id: 4
+  },
+];
